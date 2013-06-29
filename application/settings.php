ec18879557ec7bc95d1280c3cc6ceaf8bb3f44b5
John Blossom <john@montyburns.(none)>
2013-06-29 05:54:15 -0400

    distributable settings

--- application/settings.php
+++ application/settings.php
@@ -4,11 +4,11 @@ ini_set('display_errors', 1);
 
 
 define('SITE_NAME', 'Demo Site');
-define('CART_COOKIE', 'jblossom_demo_site_cart'); // rename this to invalidate all cookies.
+define('CART_COOKIE', 'jbdemo_site_cart'); // rename this to invalidate all cookies.
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'demo');
-define('DB_USER', 'demo');
-define('DB_PASSWORD', 'qQCBa2bsEYu7n6AC');
+define('DB_NAME', 'jbdemo');
+define('DB_USER', 'jbdemo');
+define('DB_PASSWORD', 'jbdemo');
 define('DB_PORT', '3306');
 define('DB_CHARSET', 'utf8');
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is a simple proof of concept
+Using CodeIgniter and Bootstrap in context of E-Commerce
+
+Deploy code, run SQL. 
+Make sure mod_rewrite is installed and httpd.conf points at correct .so
